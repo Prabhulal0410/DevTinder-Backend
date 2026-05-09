968dd56039072c6bb6c33ca4f3e4b83630e8d78c
Prabhulal <prabhulal.raghwani410@gmail.com>
2026-05-09 22:24:34 +0530

    learn about routes and query and params

--- src/app.js
+++ src/app.js
@@ -6,6 +6,16 @@ app.get("/", (req, res) => {
   res.send("Server is running");
 });
 
+// app.get("/user",(req,res) => {
+//   console.log(req.query)
+//   res.send("user log")
+// })
+
+app.get("/user/:id",(req,res) => {
+  console.log(req.params)
+  res.send("user log")
+})
+
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
