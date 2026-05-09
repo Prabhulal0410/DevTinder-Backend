d9a867af12b595d82d4f9d6c7a820f7e46a65aa7
Prabhulal <prabhulal.raghwani410@gmail.com>
2026-05-09 23:01:00 +0530

    learn about multiple route handler and next() function

--- src/app.js
+++ src/app.js
@@ -2,19 +2,22 @@ import express from "express";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Server is running");
-});
+// app.get("/", (req, res) => {
+//   res.send("Server is running");
+// });
 
-// app.get("/user",(req,res) => {
-//   console.log(req.query)
+app.get("/user",(req,res,next) => {
+  console.log("1st handler")
+  next()
+  // express give use next() which is call next route handler 
+},(req,res)=>{
+  res.send("2nd handler")
+})
+
+// app.get("/user/:id",(req,res) => {
+//   console.log(req.params)
 //   res.send("user log")
 // })
-
-app.get("/user/:id",(req,res) => {
-  console.log(req.params)
-  res.send("user log")
-})
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
