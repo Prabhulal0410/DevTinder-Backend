276464a77d17507822a834e0960aa3817aad7410
Prabhulal <prabhulal.raghwani410@gmail.com>
2026-05-10 22:16:19 +0530

    started leaning about middleware and Auth

--- src/app.js
+++ src/app.js
@@ -2,22 +2,29 @@ import express from "express";
 
 const app = express();
 
-// app.get("/", (req, res) => {
-//   res.send("Server is running");
-// });
+// app.get("/user",(req,res,next) => {
+//   console.log("1st handler")
+//   next()
+//   // express give use next() which is call next route handler 
+// },(req,res)=>{
+//   res.send("2nd handler")
+// })
 
-app.get("/user",(req,res,next) => {
-  console.log("1st handler")
-  next()
-  // express give use next() which is call next route handler 
-},(req,res)=>{
-  res.send("2nd handler")
+// Handle Auth MIddleware for all POST,GET,PUT,DELETE
+app.use("/admin",(req,res,next)=>{
+  const token = "abc"
+  const isAdminAuth = token ==="abc"
+  if(!isAdminAuth){
+    res.status(401).send("unauthorized request")
+  }else{
+    next()
+  }
 })
 
-// app.get("/user/:id",(req,res) => {
-//   console.log(req.params)
-//   res.send("user log")
-// })
+app.get("admin/getAllData",(req,res)=>{
+  res.send("All Data")
+})
+
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
