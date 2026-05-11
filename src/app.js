40e211acd2766c32bf72a061d6a19f2a0e858f71
Prabhulal <prabhulal.raghwani410@gmail.com>
2026-05-11 23:07:32 +0530

    create 1st user and check in mongodb compass

--- src/app.js
+++ src/app.js
@@ -1,8 +1,27 @@
 import express from "express";
 import {adminAuth} from "./middlewares/auth.js"
 import {connectDB} from "./config/database.js"
+import { User } from "./models/user.js";
 
 const app = express();
+
+
+app.post("/signup",async (req,res)=>{
+  //creating new instance of the User model
+const user = new User({
+    firstName:"prabhulal",
+    lastName:"raghwani",
+    emailId:"prabhulal@gmail.com",
+    password:"123456"
+  })
+
+  await user.save()
+  res.send("user Added")
+})
+
+
+
+
 
 
 // Connect DB first
