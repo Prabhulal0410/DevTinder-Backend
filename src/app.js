a60fcd4719860cdee20e7f18cc4c84435318b766
Prabhulal <prabhulal.raghwani410@gmail.com>
2026-05-11 22:12:27 +0530

    connect db and server

--- src/app.js
+++ src/app.js
@@ -5,7 +5,19 @@ import {connectDB} from "./config/database.js"
 const app = express();
 
 
+// Connect DB first
+connectDB()
+  .then(() => {
+    console.log("Database connection established.");
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
-});
+    app.listen(3000, () => {
+      console.log("Server running on port 3000");
+    });
+  })
+  .catch((err) => {
+    console.log("Database cannot be connected!!");
+    console.error(err);
+  });
+
+  // First we have to connect with db
+  // then will start server and listen
--- src/config/database.js
+++ src/config/database.js
@@ -4,11 +4,5 @@ export const connectDB = async() => {
     await mongoose.connect("mongodb+srv://prabhulalraghwani410_db:QEWzn7foalrS8RVY@cluster.cbmcbze.mongodb.net/devTinder")
 }
 
-// devtinder is the name of database 
+// devTinder is the name of database 
 // if you dont put somethign after .net/ it will refer to cluster whic we create in atlas
-
-connectDB().then(()=>{
-    console.log("databse connection established.")
-}).catch(err=>{
-    console.log(err)
-})
