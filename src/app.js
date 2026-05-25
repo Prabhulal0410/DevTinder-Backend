71a18125121e38b8bd90005e730d8ae469ff2285
Prabhulal <prabhulal.raghwani410@gmail.com>
2026-05-25 22:58:57 +0530

    learn about how to generate token and get profile based on token and id

--- src/app.js
+++ src/app.js
@@ -1,32 +1,33 @@
 import express from "express";
-import {adminAuth} from "./middlewares/auth.js"
-import {connectDB} from "./config/database.js"
+import { adminAuth } from "./middlewares/auth.js";
+import { connectDB } from "./config/database.js";
 import { User } from "./models/user.js";
 import { validateSignUpData } from "./utils/validation.js";
-import bcrypt from "bcrypt"
-import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import cookieParser from "cookie-parser";
 
 const app = express();
 
 // express give us this methode so that express body understand Body obj
-app.use(express.json())
+app.use(express.json());
 // cookieParser will read tha cookie data which sent by client.(its a middleware)
-app.use(cookieParser())
+app.use(cookieParser());
 
 app.post("/signup", async (req, res) => {
-
   try {
     // validate Data
-    validateSignUpData(req)
+    validateSignUpData(req);
 
-    const {firstName,lastName,emailId,password} = req.body
+    const { firstName, lastName, emailId, password } = req.body;
     // Encrypt the password
-    const passwordHash = await bcrypt.hash(password,10)
+    const passwordHash = await bcrypt.hash(password, 10);
 
     const user = new User({
-      firstName,lastName,emailId,
-      password:passwordHash
+      firstName,
+      lastName,
+      emailId,
+      password: passwordHash,
     });
     await user.save();
     res.status(201).send("User added successfully");
@@ -35,91 +36,106 @@ app.post("/signup", async (req, res) => {
   }
 });
 
-app.post("/login",async(req,res)=>{
+app.post("/login", async (req, res) => {
   try {
-    const {emailId,password} = req.body
+    const { emailId, password } = req.body;
 
-    const user = await User.findOne({emailId:emailId})
-    if(!user){
-      throw new Error("Invalid credentials")
+    const user = await User.findOne({ emailId: emailId });
+    if (!user) {
+      throw new Error("Invalid credentials");
     }
 
-    const isPasswordValid = await bcrypt.compare(password,user.password)
-    if(isPasswordValid){
-
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (isPasswordValid) {
       // create jwt token
-      const token = await jwt.sign({_id:user._id},"prabhulaltoken2304") 
+      const token = await jwt.sign({ _id: user._id }, "prabhulaltoken2304");
 
       // send token in cookie
-      res.cookie("token",token)
-      res.send("Login Successfull!!!")
-    }else{
-      throw new Error("Invalid credentials")
+      res.cookie("token", token);
+      res.send("Login Successfull!!!");
+    } else {
+      throw new Error("Invalid credentials");
     }
-
   } catch (error) {
     res.status(400).send(error.message);
   }
-})
+});
 
 // profile
-app.get("/profile",async (req,res)=>{
-  const cookies = req.cookies
+app.get("/profile", async (req, res) => {
+  try {
+    const cookies = req.cookies;
 
-  const {token} = cookies
+    const { token } = cookies;
+    if (!token) {
+      throw new Error("Invalid Token");
+    }
 
-  // validate token
-  const decodedMsg = await jwt.verify(token,"prabhulaltoken2304")
-  console.log(decodedMsg)
-  
-const {_id} = decodedMsg
-console.log("Logged in user is :" + _id)
-
-  res.send(token)
-})
+    // validate token
+    const decodedMsg = await jwt.verify(token, "prabhulaltoken2304");
+    const { _id } = decodedMsg;
+    const user = await User.findById(_id);
+    if(!user){
+      throw new Error("User Tot Found")
+    }
+    res.send(user);
+  } catch (error) {
+    res.send("Error : " + error.message);
+  }
+});
 
 // Feed api - get all the user
-app.get("/feed",async (req,res)=>{
+app.get("/feed", async (req, res) => {
   try {
-    const users= await User.find({})
-    res.send(users)
+    const users = await User.find({});
+    res.send(users);
   } catch (error) {
-    res.status(400).send("Somethign went wrong")
+    res.status(400).send("Somethign went wrong");
   }
-})
+});
 
 // Delete user Api
-app.delete("/user",async(req,res)=>{
-   const userId = req.body.userId
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
   try {
-    const user = await User.findByIdAndDelete(userId)
-    res.send("User Deleted Succeffully")
+    const user = await User.findByIdAndDelete(userId);
+    res.send("User Deleted Succeffully");
   } catch (error) {
-    res.status(400).send("Somethign went wrong")
+    res.status(400).send("Somethign went wrong");
   }
-})
+});
 
-app.patch("/user/:userId",async(req,res)=>{
-  const userId = req.params?.userId
-  const data = req.body
+app.patch("/user/:userId", async (req, res) => {
+  const userId = req.params?.userId;
+  const data = req.body;
 
   try {
-  const allowed_updates= ["photoUrl","about","gender","age","skills","firstName"]
-  const isUpdatedAllowed = Object.keys(data).every((k)=>allowed_updates.includes(k))
-  if(!isUpdatedAllowed){
-    throw new Error("Update not allowed")
-  }
+    const allowed_updates = [
+      "photoUrl",
+      "about",
+      "gender",
+      "age",
+      "skills",
+      "firstName",
+    ];
+    const isUpdatedAllowed = Object.keys(data).every((k) =>
+      allowed_updates.includes(k),
+    );
+    if (!isUpdatedAllowed) {
+      throw new Error("Update not allowed");
+    }
 
-    await User.findByIdAndUpdate({_id:userId},data,{returnDocument:"before",runValidators:true},
-    )
+    await User.findByIdAndUpdate({ _id: userId }, data, {
+      returnDocument: "before",
+      runValidators: true,
+    });
     //returnDocument:"before" will return user before update and if we user "after" then it will return obj after update doc byDefault it is before if we wont pass anything
     // runValidators unable to run validation function whic is define in schema bydefault it willonly for create if use runValidators:true then only run for update &patch
-    res.send("User Updated Succeffully")
+    res.send("User Updated Succeffully");
   } catch (error) {
-    res.status(400).send("Update Failed:" + error.message)
+    res.status(400).send("Update Failed:" + error.message);
   }
-})
-
+});
 
 // Connect DB first
 connectDB()
@@ -135,5 +151,5 @@ connectDB()
     console.error(err);
   });
 
-  // First we have to connect with db
-  // then will start server and listen
+// First we have to connect with db
+// then will start server and listen
