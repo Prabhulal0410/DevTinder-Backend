0e3ce4dd5368050582e230455162483dbb000866
Prabhulal <prabhulal.raghwani410@gmail.com>
2026-07-02 21:25:24 +0530

    change db password and reconnet

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,23 @@
+// import mongoose from "mongoose";
+
+// export const connectDB = async() => {
+//     await mongoose.connect("mongodb+srv://prabhulalraghwani410_db:QEWzn7foalrS8RVY@cluster.cbmcbze.mongodb.net/devTinder")
+// }
+
+
 import mongoose from "mongoose";
 
-export const connectDB = async() => {
-    await mongoose.connect("mongodb+srv://prabhulalraghwani410_db:QEWzn7foalrS8RVY@cluster.cbmcbze.mongodb.net/devTinder")
-}
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://prabhulalraghwani410_db:Vkb!5pmgMBbbWcT@cluster.cbmcbze.mongodb.net/devTinder"
+    );
+
+    console.log("✅ MongoDB Connected");
+  } catch (err) {
+    console.error("❌ MongoDB Error:", err);
+  }
+};
 
 // devTinder is the name of database 
 // if you dont put somethign after .net/ it will refer to cluster whic we create in atlas
