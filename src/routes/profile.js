fc817b4b4ace9fbfa5291c4f403e357f7f953b43
Prabhulal <prabhulal.raghwani410@gmail.com>
2026-07-07 22:49:56 +0530

    created profile edit api

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,10 +1,11 @@
 import express from "express"
 import { userAuth } from "../middlewares/auth.js";
+import { validateEditProfileData } from "../utils/validation.js";
 
 const profileRouter = express.Router()
 
 // profile
-profileRouter.get("/profile", userAuth, async (req, res) => {
+profileRouter.get("/profile/view", userAuth, async (req, res) => {
   try {
     const user = req.user;
     res.send(user);
@@ -12,5 +13,25 @@ profileRouter.get("/profile", userAuth, async (req, res) => {
     res.send("Error : " + error.message);
   }
 });
+
+profileRouter.patch("/profile/edit",userAuth,async(req,res)=>{
+  try {
+    if(!validateEditProfileData(req)){
+      throw new Error("Invalid Edit request")
+    }
+
+    // loggedIn user
+    const loggedInUser = req.user
+    // loggedInUser.firstName = req.body.firstName
+    Object.keys(req.body).forEach((key) => (loggedInUser[key] = req.body[key]))
+
+    await loggedInUser.save(); 
+
+    res.send(`${loggedInUser.firstName} your profile updated successfully`)
+
+  } catch (error) {
+    res.status(400).send("Error : " + error.message) 
+  }
+})
 
 export default profileRouter
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -9,3 +9,18 @@ export const validateSignUpData = (req) =>{
         throw new Error("Email is required")
     }
 }
+
+
+export const validateEditProfileData = (req) => {
+  const allowedEditFields = [
+    "firstName",
+    "lastName",
+    "about",
+    "skills",
+    "gender",
+  ];
+
+  return Object.keys(req.body).every((field) =>
+    allowedEditFields.includes(field)
+  );
+};
