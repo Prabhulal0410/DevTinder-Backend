9f2271f8cd3a8978daa4b156d7eaa0f8a6c01638
Prabhulal <prabhulal.raghwani410@gmail.com>
2026-07-09 22:03:24 +0530

    added pre function in requestconnection schema

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -19,4 +19,16 @@ const connectionRequestSchema = new mongoose.Schema({
     }
 },{timestamps:true})
 
+
+// this is called before we save the data.
+connectionRequestSchema.pre("save",function(next){
+    const connectionRequest = this
+
+    // check if the fromUserId is same as toUserId
+    if(connectionRequest.fromUserId.equals(connectionRequest.toUserId)){
+        throw new Error("cannot send request to youself")
+    }
+    next()
+})
+
 export const ConnectionRequest = new mongoose.model("ConnectionRequest",connectionRequestSchema)
