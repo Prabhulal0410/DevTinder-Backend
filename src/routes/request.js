2a3f4f53f4c9f2b98da93f8b0086cd0d1959c972
Prabhulal <prabhulal.raghwani410@gmail.com>
2026-07-10 22:16:27 +0530

    build api for accept or reject

--- src/routes/request.js
+++ src/routes/request.js
@@ -90,4 +90,47 @@ requestRouter.post(
   },
 );
 
+
+
+// API for accept/reject request (who receives request)
+requestRouter.post("request/review/:status/:requestId",userAuth,async(req,res)=>{
+
+  try {
+    
+    const loggedInUser = req.user
+    const {status,requestId} = res.params
+
+    const allowedStatus = ["accepted","rejected"];
+      if(!allowedStatus.includes(status)){
+        return res.status(400).json({
+          message:"Status not allowed!"
+        })
+      }
+    
+      const connectionRequest = await ConnectionRequest.findOne({
+        _id:requestId,
+        toUserId:loggedInUser._id,
+        status:"intrested",
+      })
+      if(!connectionRequest){
+        return res.status(404).json({
+          message:"Connection request not found"
+        })
+      }
+
+
+      connectionRequest.status = status
+
+      const data = await connectionRequest.save()
+      res.json({
+        message:"Connection request "
+ + status , data  })
+
+
+  } catch (error) {
+    res.status(400).send("Error : " + error.message)
+  }
+
+})
+
 export default requestRouter;
