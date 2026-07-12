42dcb93d6ef5ca5645199f80ea26c8ef1706f801
Prabhulal <prabhulal.raghwani410@gmail.com>
2026-07-12 22:07:36 +0530

    created api for users connection

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,32 +1,53 @@
 import express from "express";
 import { userAuth } from "../middlewares/auth.js";
 import { ConnectionRequest } from "../models/connectionRequest.js";
- const userRouter = express.Router()
+const userRouter = express.Router();
+
+const USER_SAFE_DATA = "firstName lastName photoUrl age gender skills";
 
 // get all the pending connection request for the logged in user
-userRouter.get("/user/requests/received",userAuth,async(req,res)=>{
+userRouter.get("/user/requests/received", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
 
-    try {
-        const loggedInUser = req.user
+    const connectionRequests = await ConnectionRequest.find({
+      toUserId: loggedInUser._id,
+      status: "intrested",
+    }).populate("fromUserId", ["firstName", "lastName", "photoUrl"]);
+    // }).populate("fromUserId", "firtName lastName") this is also one way to write filters
 
-        const connectionRequests = await ConnectionRequest.find({
-            toUserId : loggedInUser._id,
-            status:"intrested",
-        }).populate("fromUserId",["firstName","lastName","photoUrl"])
-        // }).populate("fromUserId", "firtName lastName") this is also one way to write filters
+    res.json({
+      message: "Data Fetched succsfully!!",
+      connectionRequests,
+    });
+  } catch (error) {
+    res.status(400).send("Error : " + error.message);
+  }
+});
 
-        res.json({
-            message:"Data Fetched succsfully!!",
-            connectionRequests
-        })
+// api for user to show who is connected
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
 
-    } catch (error) {
-        res.status(400).send("Error : " + error.message)
-    }
+    const connectionRequests = await ConnectionRequest.find({
+      $or: [
+        { toUserId: loggedInUser._id, status: "accepted" },
+        { fromUserId: loggedInUser._id, status: "accepted" },
+      ],
+    }).populate("fromUserId", USER_SAFE_DATA);
 
-})
+    const data = connectionRequests.map((row) => row.fromUserId);
 
+    res.json({
+      message: "Succesfully fetch Accepted connection request",
+      data,
+    });
+  } catch (error) {
+    res.status(400).send({
+      message: error.message,
+    });
+  }
+});
 
-
-
- export default userRouter
+export default userRouter;
