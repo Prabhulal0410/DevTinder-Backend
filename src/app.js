07ba3d72b202fdd93ad121271597b35bcfb060ce
Prabhulal <prabhulal.raghwani410@gmail.com>
2026-07-29 21:35:48 +0530

    small change in app file

--- src/app.js
+++ src/app.js
@@ -25,10 +25,10 @@ app.use(express.json());
 app.use(cookieParser());
 
 
-app.use((req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log(`${req.method} ${req.url}`);
+//   next();
+// });
 
 // Use Routes
 app.use("/", authRouter);
